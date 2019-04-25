66e099cbf400b5c233008f372ab0bbc50851b712
MrVu <chuvi0902@gmail.com>
2019-04-26 02:06:18 +0700

    gmail

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hellodsadasda'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
